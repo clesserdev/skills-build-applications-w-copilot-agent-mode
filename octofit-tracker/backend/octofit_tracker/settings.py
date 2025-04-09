8b1492845cca1518a4a9fcb4d6a2de9768e1a037
clesserdev <12201894+clesserdev@users.noreply.github.com>
2025-04-09 20:20:38 +0000

    Update settings and views for codespace URL and test API endpoints

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-422bvn_hxd4p=n8sr-y05&2!&*k_j*se!vosx)uqda#i!n3c4)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'literate-couscous-wrrw5w999jqcgx5-8000.app.github.dev']
 
 
 # Application definition
